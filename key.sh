2ac38633fa263aa215dd08f5510a77af356864dc
ericivan <ericivan047@gmail.com>
2018-04-25 17:23:28 +0800

    modify key name

--- key.sh
+++ key.sh
@@ -1,15 +1,14 @@
 #生成简单秘钥
-openssl genrsa -out private.pem 1024
+openssl genrsa -out id_rsa 1024
 
 #利用秘钥生成公钥
-openssl rsa -in private.pem pubout -out public.key
+openssl rsa -in private.pem pubout -out id_rsa.pub
 
 
 
 #私钥指定加密算法,密码
-openssl genrsa -aes128 -passout pass:mysecret -out private.pem 2048
+openssl genrsa -aes128 -passout pass:mysecret -out id_rsa 2048
 
 #生成公钥
-openssl rsa -in private.pem -passin pass:mysecret -pubout -out public.pub
-
+openssl rsa -in private.pem -passin pass:mysecret -pubout -out id_rsa.pub
 
