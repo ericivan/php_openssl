78a19cc52f06cd7d24481d1b50b4a42a23ffed8b
ericivan <ericivan047@gmail.com>
2018-04-25 17:42:36 +0800

    add commit

--- de_en_crypt.php
+++ de_en_crypt.php
@@ -4,10 +4,12 @@ $data='my transformer data';
 
 $publickeyFile="./id_rsa.pub";
 
+/*生成公钥的key*/
 $publicKey=openssl_pkey_get_public(file_get_contents($publickeyFile));
 
 $encryptedData='';
 
+/*加密数据*/
 openssl_public_encrypt($data,$encryptedData,$publicKey);
 
 $encryptedData=base64_encode($encryptedData);
@@ -18,10 +20,12 @@ $privateKeyFile='./id_rsa';
 
 $passphrase = 'mysecret';
 
+/*生成私钥,附带密码*/
 $privateKey=openssl_pkey_get_private(file_get_contents($privateKeyFile),$passphrase);
 
 $sensitiveData='';
 
+/*利用私钥解密数据*/
 openssl_private_decrypt(base64_decode($encryptedData),$sensitiveData,$privateKey);
 
 var_dump($sensitiveData);
--- signature.php
+++ signature.php
@@ -2,16 +2,21 @@
 
 $data = 'my signature data';
 
+/*密码*/
 $passphrase = 'mysecret';
 
+/*摘要生成算法*/
 $digestAlgo = 'sha512';
 
+/*签名算法*/
 $algo = OPENSSL_ALGO_SHA1;
 
 $privateKeyFile = './id_rsa';
 
+/*生成私钥*/
 $privateKey = openssl_pkey_get_private(file_get_contents($privateKeyFile),$passphrase);
 
+/*生成摘要*/
 if (function_exists("hash")) {
     $digest = hash($digestAlgo, $data, TRUE);
 } elseif (function_exists("mhash")) {
@@ -23,6 +28,7 @@ $digest = bin2hex($digest);
 
 $signature = '';
 
+/*签名生成*/
 openssl_sign($digest, $signature,$privateKey,$algo);
 
 $signature = base64_encode($signature);
@@ -31,10 +37,14 @@ var_dump($signature);
 
 $publicKeyFile = './id_rsa.pub';
 
+/*生成公钥*/
 $publicKey = openssl_pkey_get_public(file_get_contents($publicKeyFile));
 
+
+/*签名认证*/
 $verify = openssl_verify($digest, base64_decode($signature), $publicKey, $algo);
 
+/*输出1为签名成功*/
 var_dump($verify);
 
 
